da109caf9e82fbcf9bd6affbd348d07127bd6118
stanislasss <twolf@hartford.YAGER.local>
2020-11-18 10:53:39 +0100

    setting back to local url

--- src/grafanaSettings.py
+++ src/grafanaSettings.py
@@ -1,6 +1,6 @@
 import os
 
-GRAFANA_URL = os.getenv('GRAFANA_URL', 'https://grafana.dev-1.eu-west-1.infra.yager-development.com')
+GRAFANA_URL = os.getenv('GRAFANA_URL', 'http://localhost:3000')
 TOKEN = os.getenv('GRAFANA_TOKEN',
                   'eyJrIjoiN1lrTExOVDcycEVRWExMeUE5VHFZRng3TVh2MThOUVgiLCJuIjoidGVzdC10b29sLWJhY2t1cCIsImlkIjoxfQ==')
 HTTP_GET_HEADERS = {'Authorization': 'Bearer ' + TOKEN}
